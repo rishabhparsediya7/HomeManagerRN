01d0aee5898f7ab17b35c2d1d8baa1e1df62a7a0
Rishabh Parsediya <parsediyarishabh@gmail.com>
2025-05-31 15:30:52 +0530

    feat: add theme coloring

--- src/providers/theme.style.tsx
+++ src/providers/theme.style.tsx
@@ -1,14 +1,41 @@
 const COLORS = {
+  // Brand Colors
   primary: '#000000',
   secondary: '#000000',
   tertiary: '#000000',
   quaternary: '#000000',
   quinary: '#000000',
+
+  // Base Colors
   white: '#FFFFFF',
   black: '#000000',
+
+  primaryText: '#4B7BFF',
+
+  // Gray Scale
   gray: '#808080',
   lightGray: '#D3D3D3',
   darkGray: '#A9A9A9',
+
+  // White Shades - Backgrounds
+  bgWhite: '#FFFFFF',
+  bgSnow: '#FFFAFA',
+  bgGhostWhite: '#F8F8FF',
+  bgWhiteSmoke: '#F5F5F5',
+  bgAliceBlue: '#F0F8FF',
+  bgHoneydew: '#F0FFF0',
+  bgAzure: '#F0FFFF',
+  bgMintCream: '#F5FFFA',
+
+  // White Shades - Borders
+  borderWhite: '#FFFFFF',
+  borderSnow: '#FFFAFA',
+  borderGhostWhite: '#F8F8FF',
+  borderWhiteSmoke: '#F5F5F5',
+  borderGainsboro: '#DCDCDC',
+  borderLightGray: '#D3D3D3',
+  borderSilver: '#C0C0C0',
+  borderLightSilver: '#D9D9D9',
 };
 
 export {COLORS};
