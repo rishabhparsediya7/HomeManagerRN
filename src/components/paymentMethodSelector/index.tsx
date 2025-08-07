0596bf8d0c9ad27bfc065ba41194376b72d35ddd
Rishabh Parsediya <parsediyarishabh@gmail.com>
2025-08-07 22:07:53 +0530

    Add expense refactor

--- src/components/paymentMethodSelector/index.tsx
+++ src/components/paymentMethodSelector/index.tsx
@@ -1,11 +1,46 @@
 import React from 'react';
 import {View, Text, TouchableOpacity, StyleSheet} from 'react-native';
+import { commonStyles } from '../../utils/styles';
+
+interface PaymentMethod {
+  id: string;
+  name: string;
+  icon: any;
+}
 
 const PaymentMethodSelector = ({
   paymentMethods,
   selectedPaymentMethod,
   setSelectedPaymentMethod,
+  colors,
 }) => {
+  const styles = StyleSheet.create({
+    grid: {
+      flexDirection: 'row',
+      flexWrap: 'wrap',
+      justifyContent: 'space-between',
+      gap: 6,
+    },
+    categoryItem: {
+      width: '32%',
+      backgroundColor: colors.inputBackground,
+      paddingVertical: 16,
+      borderRadius: 12,
+      alignItems: 'center',
+      marginBottom: 12,
+    },
+    categoryLabel: {
+      marginTop: 8,
+      textAlign: 'center',
+      fontSize: 12,
+      color: colors.buttonText,
+      ...commonStyles.textDefault,
+    },
+    selectedCategoryItem: {
+      borderColor: colors.buttonText,
+      borderWidth: 1,
+    },
+  });
   return (
     <View style={styles.grid}>
       {paymentMethods.map((method, idx) => {
@@ -28,30 +63,6 @@ const PaymentMethodSelector = ({
   );
 };
 
-const styles = StyleSheet.create({
-  grid: {
-    flexDirection: 'row',
-    flexWrap: 'wrap',
-    justifyContent: 'space-between',
-    gap: 6,
-  },
-  categoryItem: {
-    width: '32%',
-    backgroundColor: '#F5F6FA',
-    paddingVertical: 16,
-    borderRadius: 12,
-    alignItems: 'center',
-    marginBottom: 12,
-  },
-  categoryLabel: {
-    marginTop: 8,
-    textAlign: 'center',
-    fontSize: 12,
-  },
-  selectedCategoryItem: {
-    borderColor: '#007BFF',
-    borderWidth: 1,
-  },
-});
+
 
 export default PaymentMethodSelector;
