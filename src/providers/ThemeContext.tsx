7c17a828b53f74497ec99a08c2b1267572bb9fd6
Rishabh Parsediya <parsediyarishabh@gmail.com>
2025-08-08 16:28:09 +0530

    change the default theme to dark

--- src/providers/ThemeContext.tsx
+++ src/providers/ThemeContext.tsx
@@ -7,12 +7,12 @@ type ThemeContextType = {
 };
 
 const ThemeContext = createContext<ThemeContextType>({
-  theme: 'light',
+  theme: 'dark',
   toggleTheme: () => {},
 });
 
 export const ThemeProvider = ({ children }: { children: React.ReactNode }) => {
-  const [theme, setTheme] = useState<ColorSchemeName>(Appearance.getColorScheme());
+  const [theme, setTheme] = useState<ColorSchemeName>('dark');
 
   useEffect(() => {
     const listener = Appearance.addChangeListener(({ colorScheme }) => {
