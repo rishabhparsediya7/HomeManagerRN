7bc16887a08380d248e9039d43fe605eaa1f1a37
Rishabh Parsediya <parsediyarishabh@gmail.com>
2025-08-08 20:32:50 +0530

    fix: metro and build level changes

--- metro.config.js
+++ metro.config.js
@@ -1,11 +1,20 @@
-const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
+const { getDefaultConfig, mergeConfig } = require('@react-native/metro-config');
 
-/**
- * Metro configuration
- * https://reactnative.dev/docs/metro
- *
- * @type {import('@react-native/metro-config').MetroConfig}
- */
-const config = {};
+const defaultConfig = getDefaultConfig(__dirname);
 
-module.exports = mergeConfig(getDefaultConfig(__dirname), config);
+const customConfig = {
+  transformer: {
+    getTransformOptions: async () => ({
+      transform: {
+        experimentalImportSupport: false,
+        inlineRequires: true,
+      },
+    }),
+  },
+  resolver: {
+    assetExts: defaultConfig.resolver.assetExts,
+    sourceExts: defaultConfig.resolver.sourceExts,
+  },
+};
+
+module.exports = mergeConfig(defaultConfig, customConfig);
