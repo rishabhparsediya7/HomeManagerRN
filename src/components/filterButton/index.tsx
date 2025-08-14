d0d67985a403ac9f6ed3e291c07edd952a8eccd6
Rishabh Parsediya <parsediyarishabh@gmail.com>
2025-08-15 02:24:19 +0530

    refactor: filter button

--- src/components/filterButton/index.tsx
+++ src/components/filterButton/index.tsx
@@ -4,7 +4,6 @@ import LinearGradient from 'react-native-linear-gradient';
 import { commonStyles } from '../../utils/styles';
 
 const FilterButton = ({ label, selected, onPress, colors }: any) => {
-
   const styles = useMemo(() => StyleSheet.create({
     button: {
       paddingVertical: 6,
@@ -28,8 +27,8 @@ const FilterButton = ({ label, selected, onPress, colors }: any) => {
   return (
     <LinearGradient
       start={{ x: 1, y: 0 }}
-      end={{ x: 0, y: 0 }}
-      colors={[colors.primary, selected ? colors.buttonTextSecondary : colors.primaryLight]}
+      end={{ x: 0, y: 1 }}
+      colors={[colors.inputBackground, selected ? colors.inputText : colors.inputBackground]}
       style={[styles.button, selected && styles.selectedButton]}
     >
       <TouchableOpacity
