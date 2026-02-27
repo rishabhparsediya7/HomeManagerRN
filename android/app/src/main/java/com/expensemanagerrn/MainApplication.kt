171b43e5e4c5b7ccd5299ceb045d04dd9ea51d4a
Rishabh Parsediya <parsediyarishabh@gmail.com>
2026-02-27 17:28:25 +0530

    build(android): configure native notification channel and build settings

--- android/app/src/main/java/com/expensemanagerrn/MainApplication.kt
+++ android/app/src/main/java/com/expensemanagerrn/MainApplication.kt
@@ -1,6 +1,9 @@
 package com.expensemanagerrn
 
 import android.app.Application
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.os.Build
 import com.facebook.react.PackageList
 import com.facebook.react.ReactApplication
 import com.facebook.react.ReactHost
@@ -36,8 +39,21 @@ class MainApplication : Application(), ReactApplication {
     super.onCreate()
     SoLoader.init(this, OpenSourceMergedSoMapping)
     if (BuildConfig.IS_NEW_ARCHITECTURE_ENABLED) {
-      // If you opted-in for the New Architecture, we load the native entry point for this app.
       load()
+    }
+
+    // Create notification channel for Android 8+ (API 26+)
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+      val channel = NotificationChannel(
+        "default",
+        "Default Notifications",
+        NotificationManager.IMPORTANCE_HIGH
+      ).apply {
+        description = "Default notification channel for push notifications"
+        enableVibration(true)
+      }
+      val notificationManager = getSystemService(NotificationManager::class.java)
+      notificationManager.createNotificationChannel(channel)
     }
   }
 }
